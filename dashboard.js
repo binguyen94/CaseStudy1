7684056bef30c529bcb30d73446f2eafb70c11fa
binguyen94 <nhannguyen.tnnnnn@gmail.com>
2023-05-03 09:06:39 +0700

    up

---
+++ dashboard.js
@@ -0,0 +1,327 @@
+// class Contact{
+//     constructor(id, name, avatar, mobile, email){
+//         this.id = id;
+//         this.name = name;
+//         this.avatar = avatar;
+//         this.mobile = mobile;
+//         this.email = email;
+//     }
+// }
+
+// const contact_key = "data-contact";
+// const avatar_url = 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar';
+// var contacts = []
+
+// function init(){
+//     if(localStorage.getItem(contact_key) == null){
+//         contacts = [
+//             new Contact(1, "Dũng", `${avatar_url}/1.jpg`, '0935123123', 'dung@gmail.com'),
+//             new Contact(2, "Cường", `${avatar_url}/2.jpg`, '0935123123', 'cuong@gmail.com'),
+//             new Contact(3, "Luyn", `${avatar_url}/3.jpg`, '0935123123', 'luyn@gmail.com'),
+//             new Contact(4, "Hoan", `${avatar_url}/4.jpg`, '0935123123', 'hoan@gmail.com'),
+//             new Contact(5, "Lợi", `${avatar_url}/5.jpg`, '0935123123', 'loi@gmail.com'),
+//             new Contact(6, "Duy", `${avatar_url}/6.jpg`, '0935123123', 'dung@gmail.com')
+//         ];
+
+//         localStorage.setItem(contact_key, JSON.stringify(contacts));
+//     }
+//     else{
+//         contacts = JSON.parse(localStorage.getItem(contact_key));
+//     }
+// }
+
+// function renderContact(){
+//     let htmls = contacts.map(function(contact, index){
+//         return `
+//             <tr>
+//                 <td class="text-center">
+//                     <input type="checkbox">
+//                 </td>
+//                 <td>${contact.name}</td>
+//                 <td class="text-center">
+//                     <img class="avatar-sm" src="${contact.avatar}" alt="">
+//                 </td>
+//                 <td class="text-center">
+//                     ${contact.mobile}
+//                 </td>
+//                 <td>
+//                     ${contact.email}
+//                 </td>
+//                 <td class="text-center">
+//                     <i class="fa-solid fa-pencil" onclick="getContact(${contact.id})"></i>
+//                     <i class="fa fa-trash" onclick='removeContact(${index})'></i>
+//                 </td>
+//             </tr>
+//         `
+//     });
+//     document.querySelector('.table>tbody').innerHTML = htmls.join("");
+// }
+
+
+// function openModal(){
+//     document.querySelector('.modal-container').classList.add('show');
+// }
+// function closeModal(){
+//     document.querySelector('.modal-container').classList.remove('show');
+//     resetModal();
+// }
+
+// function changeAvatar(){
+//     document.querySelector('.avatar-lg').src = document.querySelector('#avatar').value || 'images/noavatar.jpg';
+// }
+
+// function addContact(){
+//     let name = document.querySelector('#name').value;
+//     let avatar = document.querySelector('#avatar').value;
+//     let mobile = document.querySelector('#mobile').value;
+//     let email = document.querySelector('#email').value;
+//     let id = findMaxId() + 1;
+
+//     let contact = new Contact(id, name, avatar, mobile, email);
+//     contacts.push(contact);
+//     localStorage.setItem(contact_key, JSON.stringify(contacts));
+//     closeModal();
+//     renderContact();
+// }
+
+
+// function resetModal(){
+//     document.querySelector('#contactId').value = "0";
+//     document.querySelector('#name').value = "";
+//     document.querySelector('#avatar').value = "";
+//     document.querySelector('#mobile').value ="";
+//     document.querySelector('#email').value = "";
+//     document.querySelector('.avatar-lg').src = "images/noavatar.jpg";
+
+//     document.querySelector('#btnUpdate').classList.add('d-none');
+//     document.querySelector('#btnAdd').classList.remove('d-none');
+
+//     document.querySelector('.modal-title').innerText = "Add Contact";
+// }
+// function findMaxId(){
+//     let max = 0;
+//     for(let contact of contacts){
+//         if(contact.id > max){
+//             max = contact.id;
+//         }
+//     }
+
+//     return max;
+// }
+
+
+// function removeContact(index){
+
+//     let confirm = window.confirm('Are you sure to remove this contact?');
+//     if(confirm){
+//         contacts.splice(index, 1);
+//         localStorage.setItem(contact_key, JSON.stringify(contacts));
+//         renderContact();
+//     }
+// }
+
+// function getContact(contactId){
+//     let contact = contacts.find(function(ct){
+//         return ct.id === contactId;
+//     })
+
+//     document.querySelector('#contactId').value = contact.id;
+//     document.querySelector('#name').value = contact.name;
+//     document.querySelector('#avatar').value = contact.avatar;
+//     document.querySelector('#mobile').value = contact.mobile;
+//     document.querySelector('#email').value = contact.email;
+//     document.querySelector('.avatar-lg').src = contact.avatar;
+
+//     document.querySelector('#btnUpdate').classList.remove('d-none');
+//     document.querySelector('#btnAdd').classList.add('d-none');
+
+//     document.querySelector('.modal-title').innerText = "Update Contact";
+//     openModal();
+// }
+
+// function updateContact(){
+//     let id = document.querySelector('#contactId').value;
+
+//     let contact = contacts.find(function(ct){
+//         return ct.id == id;
+//     })
+
+//     contact.name = document.querySelector('#name').value;
+//     contact.avatar = document.querySelector('#avatar').value;
+//     contact.mobile = document.querySelector('#mobile').value;
+//     contact.email = document.querySelector('#email').value;
+
+//     localStorage.setItem(contact_key, JSON.stringify(contacts));
+
+//     closeModal();
+//     renderContact();
+// }
+
+// function main(){
+//     init();
+//     renderContact();
+// }
+
+// main();
+
+class Add {
+    constructor(id, name, avatar, price, info) {
+        this.id = id;
+        this.name = name;
+        this.avatar = avatar;
+        this.price = price;
+        this.info = info;
+    }
+}
+
+let add_key = 'data-add';
+let avatar_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9AxdZZFXa2k6R9ygsvSTIonXmKFFZB0iPIQ&usqp=CAU";
+var adds = []
+
+function init() {
+    if (localStorage.getItem(add_key) == null) {
+        adds = [
+            new Add(1, "718", `${avatar_url}`, '500000', 'kỹ thuật tốt'),
+            new Add(2, "728", `${avatar_url}`, '500000', 'kỹ thuật vui'),
+            new Add(3, "719", `${avatar_url}`, '500000', 'kỹ thuật lành'),
+            new Add(4, "Caya", `${avatar_url}`, '500000', 'kỹ thuật an'),
+            new Add(5, "720", `${avatar_url}`, '500000', 'kỹ thuật mây'),
+            new Add(6, "740", `${avatar_url}`, '500000', 'kỹ thuật lạ'),
+        ];
+
+        localStorage.setItem(add_key, JSON.stringify(adds));
+    }
+    else {
+        adds = JSON.parse(localStorage.getItem(add_key));
+    }
+}
+
+function renderAdd() {
+    let htmls = adds.map(function (add, index) {
+        return `
+                <tr>
+                    <td>
+                        <input type="checkbox">
+                    </td>
+                    <td>${add.name}</td>
+                    <td><img style="width:150px" clas="avatar-sm" src="${add.avatar}" alt=""></td>
+                    <td>${add.price}</td>
+                    <td>${add.info}</td>
+                    <td>
+                        <i class="fa-solid fa-pencil" onclick = "getAdd(${add.id})"></i>
+                        <i class="fa fa-trash" onclick = "removeAdd(${index})"></i>
+                    </td>
+                </tr>
+            `
+    });
+    document.querySelector('.table>tbody').innerHTML = htmls.join("");
+}
+
+
+function openModel() {
+    document.querySelector('.model-container').classList.add('show');
+}
+function closeModel() {
+    document.querySelector('.model-container').classList.remove('show');
+    resetModel();
+}
+
+function changeAvatar() {
+    document.querySelector('.avatar-lg').src = document.querySelector('#avatar').value || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9AxdZZFXa2k6R9ygsvSTIonXmKFFZB0iPIQ&usqp=CAU';
+}
+
+function addProduct() {
+    let name = document.querySelector('#name').value
+    let avatar = document.querySelector('#avatar').value
+    let price = document.querySelector('#price').value
+    let info = document.querySelector('#info').value
+    let id = findMaxId() + 1;
+
+    let add = new Add(id, name, avatar, price, info);
+    adds.push(add);
+    localStorage.setItem(add_key, JSON.stringify(adds));
+    closeModel();
+    renderAdd();
+}
+
+function resetModel() {
+    document.querySelector('#addId').value = "0";
+    document.querySelector('#name').value = "";
+    document.querySelector('#avatar').value = "";
+    document.querySelector('#price').value = "";
+    document.querySelector('#info').value = "";
+    document.querySelector('.avatar-lg').src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9AxdZZFXa2k6R9ygsvSTIonXmKFFZB0iPIQ&usqp=CAU";
+
+    document.querySelector('#btnUpdate').classList.add('d-none');
+    document.querySelector('#btnAdd').classList.remove('d-none');
+    document.querySelector('.model-title').innerText = "Add Product";
+}
+function findMaxId() {
+    let max = 0;
+    for (let add of adds) {
+        if (add.id > max) {
+            max = add.id
+        }
+    }
+    return max
+}
+
+function removeAdd(index) {
+
+    let confirm = window.confirm("Bạn có chắc chắn xóa sản phẩm này không?");
+    if (confirm) {
+        adds.splice(index, 1);
+        localStorage.setItem(add_key, JSON.stringify(adds));
+        renderAdd();
+    }
+
+}
+
+function getAdd(addId) {
+    let add = adds.find(function (ad) {
+        return ad.id === addId;
+    })
+
+    document.querySelector('#addId').value = add.id;
+    document.querySelector('#name').value = add.name;
+    document.querySelector('#avatar').value = add.avatar;
+    document.querySelector('#price').value = add.price;
+    document.querySelector('#info').value = add.info;
+    document.querySelector('.avatar-lg').src = add.avatar;
+
+    document.querySelector('#btnUpdate').classList.remove('d-none');
+    document.querySelector('#btnAdd').classList.add('d-none');
+
+    document.querySelector('.model-title').innerText = "Sửa Sản Phẩm";
+    openModel();
+
+}
+
+function updateProduct() {
+
+    let id = document.querySelector('#addId').value
+
+    let add = adds.find(function (ad) {
+        return ad.id == id;
+    })
+    add.name = document.querySelector('#name').value
+    add.avatar = document.querySelector('#avatar').value
+    add.price = document.querySelector('#price').value
+    add.info = document.querySelector('#info').value
+    localStorage.setItem(add_key, JSON.stringify(adds));
+    closeModel();
+    renderAdd();
+    
+}
+
+function main() {
+    init();
+    renderAdd()
+}
+main()
+
+//link web tổng
+let icon = document.getElementById(".logo");
+icon.addEventListener("click", function () {
+        window.open("http://127.0.0.1:5500/index.html", "_blank");
+});
--- main.js
+++ main.js
@@ -7,36 +7,35 @@ document.querySelector('#search-icon', 'cart-icon').onclick = () => {
 // Header
 let header = document.querySelector('header');
 
-window.addEventListener('scroll' , () => {
+window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
 
 //
 let btn = document.querySelectorAll('.btn-buy')
 // console.log(btn)
-btn.forEach(function(button,index){
-button.addEventListener("click",function(event){
-    let btnItem = event.target
-    let product = btnItem.parentElement
-    let productImg = product.querySelector("img").src
-    let productName = product.querySelector("h3").innerText
-    let productPrice = product.querySelector('span').innerText
-    // console.log(productPrice,productImg,productName)
-    addCart(productPrice,productImg,productName)
-})
+btn.forEach(function (button, index) {
+    button.addEventListener("click", function (event) {
+        let btnItem = event.target
+        let product = btnItem.parentElement
+        let productImg = product.querySelector("img").src
+        let productName = product.querySelector("h3").innerText
+        let productPrice = product.querySelector('span').innerText
+        // console.log(productPrice,productImg,productName)
+        addCart(productPrice, productImg, productName)
+    })
 
 })
-function addCart(productPrice,productImg,productName){
+function addCart(productPrice, productImg, productName) {
     let addtr = document.createElement("tr")
     let cartItem = document.querySelectorAll("tbody tr")
-    for (let i = 0; i<cartItem.length; i++){
+    for (let i = 0; i < cartItem.length; i++) {
         let productNew = document.querySelectorAll(".title")
-        if(productNew[i].innerHTML == productName){
-            alert("Sản phẩm của bạn đã có trong giỏ hàng")
+        if (productNew[i].innerHTML == productName) {
             return
         }
     }
-    let trcontent = '<tr><td class="product1"><img class="product1-img" src="'+productImg+'" alt=""><span class="title">'+productName+'</span></td><td><p><span class="price">'+productPrice+'</span><sup>VNĐ</sup></p></td><td><input class="input-product" type="number" value="1" min="1"></td><td class="btn-del">Xóa</td></tr>'
+    let trcontent = '<tr><td class="product1"><img class="product1-img" src="' + productImg + '" alt=""><span class="title">' + productName + '</span></td><td><p><span class="price">' + productPrice + '</span><sup>VNĐ</sup></p></td><td><input class="input-product" type="number" value="1" min="1"></td><td class="btn-del">Xóa</td></tr>'
     addtr.innerHTML = trcontent
     let cartTable = document.querySelector("tbody")
     // console.log(cartTable)
@@ -47,12 +46,12 @@ function addCart(productPrice,productImg,productName){
 }
 
 // Tính tổng giá
-function carttotal (){
+function carttotal() {
     let cartItem = document.querySelectorAll("tbody tr")
     let totalPrice = 0
     // console.log(cartItem.length)
-    
-    for (let i = 0; i<cartItem.length; i++){
+
+    for (let i = 0; i < cartItem.length; i++) {
         let inputValue = cartItem[i].querySelector("input").value
         // console.log(inputValue);
         let productPrice = cartItem[i].querySelector(".price").innerText
@@ -61,14 +60,14 @@ function carttotal (){
         totalMultiply = inputValue * newsProductPrice
         // console.log(numberWithCommas(totalMultiply));
         totalPrice = totalPrice + totalMultiply
-        
-        
+
+
     }
     let carTotalPrice = document.querySelector(".price-total span")
     let cartPrice = document.querySelector(".cart-price")
     carTotalPrice.innerHTML = numberWithCommas(totalPrice)
     cartPrice.innerHTML = numberWithCommas(totalPrice)
-    inputChange ()
+    inputChange()
 }
 
 function numberWithCommas(x) {
@@ -77,40 +76,40 @@ function numberWithCommas(x) {
 
 
 //Xóa Sản phẩm
-function deleteCart(){
+function deleteCart() {
     let cartItem = document.querySelectorAll("tbody tr")
-    for (let i = 0; i<cartItem.length; i++){
+    for (let i = 0; i < cartItem.length; i++) {
         let productNew = document.querySelectorAll(".btn-del")
-        productNew[i].addEventListener("click",function(event){
+        productNew[i].addEventListener("click", function (event) {
             let cartDelete = event.target
             let cartItemRemove = cartDelete.parentElement
             cartItemRemove.remove()
-            carttotal ()
-            
+            carttotal()
+
         })
-        
+
     }
 }
 
-function inputChange () {
+function inputChange() {
     let cartItem = document.querySelectorAll("tbody tr")
-    for (let i = 0; i<cartItem.length; i++){
-        let inputValue =cartItem[i].querySelector("input")
-        inputValue.addEventListener("change",function(){
-            carttotal ();
+    for (let i = 0; i < cartItem.length; i++) {
+        let inputValue = cartItem[i].querySelector("input")
+        inputValue.addEventListener("change", function () {
+            carttotal();
         })
-        
+
     }
 }
 
 let cartBtn = document.querySelector(".bx.bx-x")
 let cartShow = document.querySelector(".bx.bx-cart")
-cartShow.addEventListener("click",function(){
-    
+cartShow.addEventListener("click", function () {
+
     document.querySelector(".cart").style.right = "0"
 })
-cartBtn.addEventListener("click",function(){
-    
+cartBtn.addEventListener("click", function () {
+
     document.querySelector(".cart").style.right = "-100%"
 })
 
@@ -118,24 +117,24 @@ cartBtn.addEventListener("click",function(){
 //
 let detailBtn1 = document.querySelector(".details1 .bx")
 let detailShow1 = document.querySelector(".details-btn1")
-detailShow1.addEventListener("click",function(){
+detailShow1.addEventListener("click", function () {
     console.log(detailShow1)
     document.querySelector(".details1").style.right = "0"
 })
-detailBtn1.addEventListener("click",function(){
+detailBtn1.addEventListener("click", function () {
     console.log(detailShow1)
-    
+
     document.querySelector(".details1").style.right = "-100%"
 })
 
 
 let detailBtn2 = document.querySelector(".details2 .bx")
 let detailShow2 = document.querySelector(".details-btn2")
-detailShow2.addEventListener("click",function(){
+detailShow2.addEventListener("click", function () {
     console.log(detailShow2)
     document.querySelector(".details2").style.right = "0"
 })
-detailBtn2.addEventListener("click",function(){
+detailBtn2.addEventListener("click", function () {
     console.log(detailShow2)
     document.querySelector(".details2").style.right = "-100%"
 })
@@ -143,49 +142,145 @@ detailBtn2.addEventListener("click",function(){
 
 let detailBtn3 = document.querySelector(".details3 .bx")
 let detailShow3 = document.querySelector(".details-btn3")
-detailShow3.addEventListener("click",function(){
+detailShow3.addEventListener("click", function () {
     console.log(detailShow3)
     document.querySelector(".details3").style.right = "0"
 })
-detailBtn3.addEventListener("click",function(){
-    
+detailBtn3.addEventListener("click", function () {
+
     document.querySelector(".details3").style.right = "-100%"
 })
 
 
 let detailBtn4 = document.querySelector(".details4 .bx")
 let detailShow4 = document.querySelector(".details-btn4")
-detailShow4.addEventListener("click",function(){
+detailShow4.addEventListener("click", function () {
     console.log(detailShow4)
     document.querySelector(".details4").style.right = "0"
 })
-detailBtn4.addEventListener("click",function(){
-    
+detailBtn4.addEventListener("click", function () {
+
     document.querySelector(".details4").style.right = "-100%"
 })
 
 
 let detailBtn5 = document.querySelector(".details5 .bx")
 let detailShow5 = document.querySelector(".details-btn5")
-detailShow5.addEventListener("click",function(){
+detailShow5.addEventListener("click", function () {
     console.log(detailShow5)
     document.querySelector(".details5").style.right = "0"
 })
-detailBtn5.addEventListener("click",function(){
-    
+detailBtn5.addEventListener("click", function () {
+
     document.querySelector(".details5").style.right = "-100%"
 })
 
 
 let detailBtn6 = document.querySelector(".details6 .bx")
 let detailShow6 = document.querySelector(".details-btn6")
-detailShow6.addEventListener("click",function(){
+detailShow6.addEventListener("click", function () {
     console.log(detailShow6)
     document.querySelector(".details6").style.right = "0"
 })
-detailBtn6.addEventListener("click",function(){
-    
+detailBtn6.addEventListener("click", function () {
+
     document.querySelector(".details6").style.right = "-100%"
 })
+
+// Tìm kiếm
+
+
+document.querySelector('#search-item').addEventListener('input', filterList)
+
+function filterList() {
+    let searchInput = document.querySelector('#search-item')
+    let filter = searchInput.value.toLowerCase()
+    let listItems = document.querySelectorAll('.producth3')
+    listItems.forEach((item) => {
+        let text = item.textContent
+        if (text.toLowerCase().includes(filter.toLowerCase())) {
+            item.style.fontSize = 'larger';
+        }
+        else {
+            item.style.fontSize = 'small';
+        }
+    });
+}
+
+//web add
+let icon = document.getElementById("add-icon");
+icon.addEventListener("click", function () {
+    var result = confirm("Bạn có muốn sang trang quản lí mặt hàng sản phẩm?");
+    if (result) {
+        window.open("http://127.0.0.1:5500/dashboard.html", "_blank");
+    }
+});
+
+
+//xem thêm
+
+
+
+let aboutBtn = document.querySelector('.about-btn');
+let moreContent = document.querySelector('#more-content');
+
+aboutBtn.addEventListener('click', function () {
+    event.preventDefault();
+    moreContent.style.display = 'block';
+    aboutBtn.style.display = 'none';
+});
+
+moreContent.addEventListener('click', function () {
+    moreContent.style.display = 'none';
+    aboutBtn.style.display = 'block';
+});
+
+
+let blog1 = document.querySelector('.blog-btn1');
+let moreContent2 = document.querySelector('#more-content2');
+
+blog1.addEventListener('click', function () {
+    event.preventDefault();
+    moreContent2.style.display = 'block';
+    blog1.style.display = 'none';
+});
+
+moreContent2.addEventListener('click', function () {
+    moreContent2.style.display = 'none';
+    blog1.style.display = 'block';
+});
+
+
+let blog2 = document.querySelector('.blog-btn2');
+let moreContent3 = document.querySelector('#more-content3');
+
+blog2.addEventListener('click', function () {
+    event.preventDefault();
+    moreContent3.style.display = 'block';
+    blog2.style.display = 'none';
+});
+
+moreContent3.addEventListener('click', function () {
+    moreContent3.style.display = 'none';
+    blog2.style.display = 'block';
+});
+
+
+let blog3 = document.querySelector('.blog-btn3');
+let moreContent4 = document.querySelector('#more-content4');
+
+blog3.addEventListener('click', function () {
+    event.preventDefault();
+    moreContent4.style.display = 'block';
+    blog3.style.display = 'none';
+});
+
+moreContent3.addEventListener('click', function () {
+    moreContent4.style.display = 'none';
+    blog3.style.display = 'block';
+});
+
+
+
 
 
